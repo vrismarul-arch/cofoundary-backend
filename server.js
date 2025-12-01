59d3d2ac244ff7c3c72369fddc4d867356a82f68
vrismarul-arch <vrismarul@gmail.com>
2025-12-01 16:25:56 +0530

    task

--- server.js
+++ server.js
@@ -56,5 +56,5 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
